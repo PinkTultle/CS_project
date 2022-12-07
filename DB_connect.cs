c58fb9ca68ba1b27e94a4e621502ce8c9694b52d
bom <bomingming>
2022-12-08 00:38:44 +0900

    백업

---
+++ DB_connect.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SMP_cs
+{
+   class DB_connect
+    {
+            string _server = "wwater.xyz"; //DB 서버 주소, 로컬일 경우 localhost
+            int _port = 31001; //DB 서버 포트
+            string _database = "todolist"; //DB 이름
+            string _id = "todolist"; //계정 아이디
+            string _pw = "yJma3IHh"; //계정 비밀번호
+            string _connectionAddress = "";
+            public MySqlConnection conn;
+
+
+            public void Open()
+            {
+                try
+                {
+                    _connectionAddress = $"Server={_server};Port={_port};Database={_database};Uid={_id};Pwd={_pw}";
+                    if (conn == null)
+                    {
+                        conn = new MySqlConnection(_connectionAddress);
+                        conn.Open();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            public void Close()
+            {
+                try
+                {
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            public void SQLQuery(string query)
+            {
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+   }
+}
---
+++ Form3.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMP_cs
+{
+    public partial class Form3 : Form
+    {
+        string sqlQuery = "";
+        DB_connect dB_Connect;
+        public Form3()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false; // 전체화면 비활성화
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 입고 버튼
+        {
+            dB_Connect = new DB_connect();
+            dB_Connect.Open();
+            sqlQuery = $"INSERT INTO Items values('{textBox4.Text}','{textBox1.Text}',{textBox3.Text},{textBox2.Text});";
+            dB_Connect.SQLQuery(sqlQuery);
+            this.Close();
+
+            // 텍스트 박스에 담긴 값 DB로 전송
+
+        }
+    }
+}
