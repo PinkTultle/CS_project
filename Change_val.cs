c4aeac2ade24ec2cb1fb317a1117c2cde4ea4432
이인석 <inseuk1007@naver.com>
2022-12-09 21:14:46 +0900

    자료형 검사 코드추가

--- Change_val.cs
+++ Change_val.cs
@@ -9,14 +9,13 @@ namespace SMP_cs
 {
     internal class Change_val
     {
-        string val_str,Rt_str;
-        int Rt_int;
-        double Rt_double;
-        bool Rt_bool;
+        string val_str;
+        bool Cheak_type;
 
         //입력 값 : val, 변형후 반환할 자료형 : type
         public object Change_type<T>(T val, string type)
         {
+            //입력받은 변수 스트링 형으로 변환
             val_str= val.ToString();
             try
             {
@@ -33,17 +32,71 @@ namespace SMP_cs
                     //double형 반환
                     case "double":
                         return double.Parse(val_str);
-
+                    //사용법이 틀린 경우 null값 반환
                     default:
                         return null;
 
                 }
             } catch
             {
+                //int >> bool / bool > int 과 같이 변환 불가능한 자료형끼리 자료형 변환 시도한 경우 예외처리
                 return null;
             }
 
         }
+
+        public object Cheak_dt<T> ( T val, string type)
+        {
+            string val_type = val.GetType().Name,
+                int_ty = "int",
+                str_ty = "str",
+                bool_ty = "bool",
+                double_ty = "double";
+            
+
+            if( type== int_ty )
+            {
+                if(val_type == "Int32")
+                {
+                    return true;
+                }
+                else return false;
+            } 
+            else if (type == str_ty)
+            {
+                if (val_type == "String")
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else if (type == bool_ty)
+            {
+                if (val_type == "Boolean")
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else if (type == double_ty)
+            {
+                if (val_type == "Double")
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else
+            {
+                return null;
+            }
+
+
+
+        }
+
+
+
 
 
 
