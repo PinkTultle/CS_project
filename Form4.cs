7ee7bdce24822d6a16a548bb9650176b5e7482d5
hiwhwnsgh <hiwhwnsgh@gmail.com>
2022-12-10 16:54:22 +0900

    출고 프로시저 호출 성공

--- Form4.cs
+++ Form4.cs
@@ -1,4 +1,5 @@
 ﻿using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
 using Org.BouncyCastle.Asn1.X509;
 using System;
 using System.Collections.Generic;
@@ -16,11 +17,7 @@ namespace SMP_cs
 {
     public partial class Form4 : Form
     {
-        string sqlQuery;
         DB_connect dB_Connect;
-        DataTable dB_dt;
-        DataTable dt_item;
-        DataTable dt_com;
         string tg_item;
         int tg_Count;
 
@@ -40,6 +37,7 @@ namespace SMP_cs
             dB_Connect = new DB_connect();
             dB_Connect.Open();
 
+
             if( (tg_Count - int.Parse(textBox3.Text)) < 0)
             {
                 MessageBox.Show("보유 수량보다 많은 값을 입력했습니다!");
@@ -47,23 +45,7 @@ namespace SMP_cs
             }
             else
             {
-                //선택 물품 출고작업
-                sqlQuery = $"UPDATE Items SET Count = Count - {textBox3.Text} WHERE Name ='{textBox1.Text}';";
-                dB_Connect.SQLQuery(sqlQuery);
-
-                /*dt_item = dB_Connect.Copy_DT(dt_item, "Items");
-                dt_com = dB_Connect.Copy_DT(dt_com, "Company");
-
-                DataRow[] dt_it = dt_item.Select($"{tg_item}");
-                DataRow[] dt_co = dt_com.Select($"{textBox2.Text}");
-                */
-
-                //Num ItemID  CompanyID Count  Date
-
-                //판매기록 데이터 테이블에 기록 추가
-                sqlQuery = $"INSERT INTO SalesRecord values({123/*Num*/},{tg_Count - int.Parse(textBox3.Text)/*Count*/},{DateTime.Now /*Date*/});";
-                dB_Connect.SQLQuery(sqlQuery);
-
+                dB_Connect.ProceDureSQLQuey("itemhistory",textBox2.Text,textBox1.Text,textBox3.Text,"출고");
                 dB_Connect.Close();
                 this.Close();
             }
