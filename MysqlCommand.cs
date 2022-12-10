cdeb8cc7ec303dc15a5bfb1d696ab6abf3192286
bom <bomingming>
2022-12-10 17:14:41 +0900

    ㅜ정

---
+++ MysqlCommand.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace SMP_cs
+{
+    internal class MysqlCommand
+    {
+        private string v;
+        private MySqlConnection conn;
+
+        public MysqlCommand(string v, MySqlConnection conn)
+        {
+            this.v = v;
+            this.conn = conn;
+        }
+
+        public static implicit operator MySqlCommand(MysqlCommand v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
